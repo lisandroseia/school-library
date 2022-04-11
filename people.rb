0a49ccd2383a258d8447222d9b7dd39e534b97c1
Leonardo <albornozleo95@gmail.com>
2022-04-11 15:19:40 -0400

    Refactor output

--- people.rb
+++ people.rb
@@ -2,30 +2,25 @@ require_relative './student'
 require_relative './teacher'
 
 class People
+  attr_reader :list
   def initialize
     @list = []
   end
 
   def add_person(person)
-    @list << person
-  end
-
-  def list_person
-    @people.each_with_index do |people, index|
-      puts "#{index}) [#{people.class.name}] Name: #{people.name} ID: #{people.id} Age: #{people.age}"
-    end
+    @list << person.create_person
   end
 end
 
 class CreatePerson
   def create_person
-    age = 0
-    until age.positive?
+    @age = 0
+    until @age.positive?
       puts 'age: '
-      age = gets.chomp.to_i
+      @age = gets.chomp.to_i
     end
     puts 'Name:'
-    name = gets.chomp.strip.capitalize
+    @name = gets.chomp.strip.capitalize
   end
 end
 
@@ -34,14 +29,14 @@ class CreateStudent < CreatePerson
     super
     permision = true
     input = ''
-    if age < 18
+    if @age < 18
       until %w[Y N].include?(input)
         puts 'has parents permission?[Y,N]'
         input = gets.chomp.upcase
         permision = input == 'Y'
       end
     end
-    Student.new(nil, age, name, parent_permission: permision)
+    Student.new(nil, @age, @name, parent_permission: permision)
   end
 end
 
@@ -50,6 +45,6 @@ class CreateTeacher < CreatePerson
     super
     puts 'Specialization:'
     specialization = gets.chomp.strip.capitalize
-    Teacher.new(specialization, age, name)
+    Teacher.new(specialization, @age, @name)
   end
 end
