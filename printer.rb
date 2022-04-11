6e02e386442dea66f62297953bfa02305109a5a4
Zeeshan Haider <zhadier39@gmail.com>
2022-04-12 01:02:36 +0500

    Fix linter errors

--- printer.rb
+++ printer.rb
@@ -1,5 +1,5 @@
 class ListPrinter
   def self.print_list(list)
-    list.each_with_index { |item, index| puts "index: #{index} #{item}"}
+    list.each_with_index { |item, index| puts "index: #{index} #{item}" }
   end
 end
