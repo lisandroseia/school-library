6d2a8c027a4e4274e6b615ac03fee9708dc8a0dc
Zeeshan Haider <zhadier39@gmail.com>
2022-04-12 22:35:18 +0500

    store rentals inside a list and handle case for exit

--- app.rb
+++ app.rb
@@ -3,11 +3,15 @@ require_relative 'rental'
 require_relative 'people'
 require_relative 'printer'
 require_relative 'bookcreator'
+require_relative 'rentalcreator'
+require_relative 'io.rb'
 
 class App
   def initialize
     @books = BookList.new
     @people = People.new
+    @rentals = RentalCreator.new
+    @manager = FileManager.new
   end
 
   def start(num)
@@ -16,9 +20,9 @@ class App
     when 2 then ListPrinter.print_list(@people.list)
     when 3 then add_person
     when 4 then @books.add_book
-    when 5 then create_rental
+    when 5 then @rentals.add_rental(@people, @books)
     when 6 then list_rentals
-    when 7 then 'exit'
+    when 7 then handle_exit
     end
   end
 
@@ -34,27 +38,6 @@ class App
     @people.add_person(input == 1 ? CreateStudent.new : CreateTeacher.new)
   end
 
-  def create_rental
-    book_index = -1
-    person_index = -1
-    puts 'select a book from the following list by number'
-    ListPrinter.print_list(@books.list)
-    until book_index > -1 && book_index < @books.list.length
-      puts 'book index:'
-      book_index = gets.chomp.to_i
-    end
-    puts 'select a person from the following list by number'
-    ListPrinter.print_list(@people.list)
-    until person_index > -1 && person_index < @people.list.length
-      puts 'person index:'
-      person_index = gets.chomp.to_i
-    end
-    puts 'date: (YYYY/MM/DD): '
-    date = gets.chomp
-    Rental.new(date, @people.filter_with_index(person_index), @books.filter_with_index(book_index))
-    puts 'Rental created succesfully'
-  end
-
   def list_rentals
     id = 0
     until id.positive?
@@ -66,5 +49,10 @@ class App
     @people.filter_with_id(id).rentals.each do |rental|
       puts "Date: #{rental.date}, book: #{rental.book.title} by #{rental.book.author}"
     end
+  end
+
+  def handle_exit
+    @manager.write(@books.list, 'books.json')
+    return 'exit'
   end
 end
